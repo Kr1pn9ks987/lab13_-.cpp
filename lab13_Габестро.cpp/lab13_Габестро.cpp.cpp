d1d09931fbd66bde58cf31682179c342602d849e
plqew <plqew@DESKTOP-C3Q2D24>
2024-05-25 00:14:50 +0300

    Добавьте файлы проекта.

---
+++ lab13_Габестро.cpp/lab13_Габестро.cpp.cpp
@@ -0,0 +1,32 @@
+﻿// lab13_Габестро.cpp.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int getSeriesElement(int n) {
+    if (n == 1) {
+        return 2;
+    }
+    else {
+        return 2 + getSeriesElement(n - 1);
+    }
+}
+
+int main() {
+    setlocale(LC_CTYPE, "ukr");
+
+    int n;
+
+    std::cout << "Введіть номер елементу ряду (n): ";
+    std::cin >> n;
+
+    if (n < 1 || n > 10) {
+        std::cerr << "Помилка: n повинно бути в діапазоні від 1 до 10" << std::endl;
+        return 1;
+    }
+
+    int result = getSeriesElement(n);
+    std::cout << "n-й член ряду: " << result << std::endl;
+
+    return 0;
+}
